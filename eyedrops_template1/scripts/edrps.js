64488df88540dbeb12680447bf54ffe0092e9eff
Owen Watson <demus2005@yahoo.com>
2013-08-27 00:27:36 -0500

    intial setup

---
+++ eyedrops_template1/scripts/edrps.js
@@ -0,0 +1,48 @@
+function mobileLogIn (username, password)
+{
+    var url ="http://www.diamondstardevelopment.com/api/MAccount/Login/";
+    url +=username+"/"+password;
+    $.get(url,
+          function(data){
+              alert(data);
+          if (data == "loggedin")
+          {
+          window.location = "today.html";
+          }
+          else if(data == "authenticated")
+          {
+          window.location = "today.html";
+          }
+          else if(data == "")
+          alert("invalid LogIn");
+          });
+}
+
+function mobileLogOut ()
+{
+    var url ="http://www.diamondstardevelopment.com/api/MAccount/Logout/logout";
+    $.get(url,
+          function(data){
+          window.location = "index.html";
+          });
+}
+
+function isLoggedIn()
+{
+    var url ="http://www.diamondstardevelopment.com/api/MAccount/Login/";
+    url +="iowen/password1";
+    $.get(url,
+          function(data){
+          if (data == "loggedin")
+          {
+          window.location = "today.html";
+          
+          }
+          else if(data == "authenticated")
+          {
+          window.location = "today.html";
+          }
+          else
+          window.location = "index.html";
+          });
+}
